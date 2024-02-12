c83ad35046452f500d0f26ea7948fc946d500abb
IT-ACA <mikeymccann@gmail.com>
2024-02-12 11:31:12 -0500

    Added page to hold calendar

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="calendar">
+      <h1>Calendar</h1>
+    </div>
+  </template>
+    
+    <style>
+    @media (min-width: 1024px) {
+      .calendar {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+      }
+    }
+    </style>
+    
